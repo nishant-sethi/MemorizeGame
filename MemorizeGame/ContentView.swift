adb71babc7b820403e15a07af529d52bd6723ac4
nishantsethi <nsethi610@gmail.com>
2023-12-17 17:18:55 +0000

    Initial Commit

---
+++ MemorizeGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemorizeGame
+//
+//  Created by Nishant sethi on 17/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Nishant sethi on 17/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
